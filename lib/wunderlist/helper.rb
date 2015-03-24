168c4f55ff36f9ba4a0f364eee6d30d25b80b5da
shun3475 <imokenpi3475@gmail.com>
2015-03-24 22:02:05 +0900

    make save and update method commonized

--- lib/wunderlist/helper.rb
+++ lib/wunderlist/helper.rb
@@ -1,12 +1,36 @@
 module Wunderlist
   module Helper
 
+    require 'active_support/inflector'
+
     def to_hash
       instance_variables = self.instance_variables
       instance_variables.delete(':@api')
       hash = {}
       instance_variables.each {|var| hash[var.to_s.delete("@")] = self.instance_variable_get(var) }
       return hash
+    end
+
+    def update
+      model_name = get_plural_model_name
+      self.api.request :put, "api/v1/#{model_name}/#{self.id}", self.to_hash
+    end
+    
+    def save
+      model_name = get_plural_model_name
+      puts model_name
+      if self.id.nil?
+        res = self.api.request :post, "api/v1/#{model_name}", self.to_hash
+      else
+        res = self.update
+      end
+      set_attrs(res)
+    end
+
+    private
+
+    def get_plural_model_name
+      self.class.to_s.gsub('Wunderlist::','').downcase.pluralize
     end
 
   end
