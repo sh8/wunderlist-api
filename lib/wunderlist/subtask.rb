7b2c6aa19a6dff55e9bda7c2621ee06dfa850f9e
shun3475 <imokenpi3475@gmail.com>
2015-03-25 12:10:37 +0900

    modify subtask.rb

--- lib/wunderlist/subtask.rb
+++ lib/wunderlist/subtask.rb
@@ -20,13 +20,13 @@ module Wunderlist
     private
 
     def set_attrs(attrs = {})
-      @id = attrs['id']
-      @api = attrs['api']
-      @task_id = attrs['task_id']
-      @created_at = attrs['created_at']
-      @created_by_id = attrs['created_by_id']
-      @revision = attrs['revision']
-      @title = attrs['title']
+      self.id = attrs['id']
+      self.api = attrs['api']
+      self.task_id = attrs['task_id']
+      self.created_at = attrs['created_at']
+      self.created_by_id = attrs['created_by_id']
+      self.revision = attrs['revision']
+      self.title = attrs['title']
     end
 
   end
