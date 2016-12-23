f16b104fd436080e77dfb4e8a0145e61370f8c35
shun3475 <imokenpi3475@gmail.com>
2016-12-24 01:37:35 +0900

    Update version to 1.1.1

--- lib/wunderlist/api.rb
+++ lib/wunderlist/api.rb
@@ -198,7 +198,7 @@ module Wunderlist
 
 
     def get_list_ids(list_names = [])
-      if list_names.is_a? Array && list_names.all? {|i| i.is_a?(Integer) }
+      if list_names.is_a? Array and list_names.all? {|i| i.is_a?(Integer) }
         return list_names
       end
       return [list_names] if list_names.is_a? Integer
